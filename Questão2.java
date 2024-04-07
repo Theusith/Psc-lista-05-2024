5a78d484da2ed2da33622f6c2e8292fd5200a3c7
Matheus Oliveira <129126932+Theusith@users.noreply.github.com>
2024-04-07 20:29:16 -0300

    Update Questão2.java

--- Questão2.java
+++ Questão2.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] array;
-        array = new int[5];
+        array = new int[10];
         Scanner sc = new Scanner(System.in);
 
 
         System.out.println("Digite 5 numeros:");
-        for(int i = 0; i < array.length; i++){
+        for(int i = 9; i  >= 0; i--){
             array[i] = sc.nextInt();
         }
         for(int num : array){
-            System.out.println(num);
+            System.out.printf(num + " " );
         }
         sc.close();
     }
